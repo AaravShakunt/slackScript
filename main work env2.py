7b2fe590354bdf9e98da6b1474494a924c4ab50e
AaravShakunt <96539960+AaravShakunt@users.noreply.github.com>
2023-03-21 19:15:42 +0530

    Added ads channel support for the script

--- main work env2.py
+++ main work env2.py
@@ -23,14 +23,19 @@ print("\nCurrent Date: ",current_date)
 UNIXTime = mktime(days_before.timetuple())
 print("7 days before current date: ",UNIXTime)
 
-CHANNEL = "C031N1UCGUS"
+CHANNEL = "YOUR CHANNEL ID"
 MESSAGES_PER_PAGE = 200
 MAX_MESSAGES = 1000
 averageReplyTime = float(0)
 numMessagesAverage= 0
 
+testerLinks = {"sheet3_endpoint": "SHEET3_ENDPOINT","sheet_endpoint": "SHEET_ENDPOINT","sheet_endpoint2": "SHEET_ENDPOINT2","channelM1":"CHANNEL1","channelM2":"CHANNEL2"}
+mainLinks = {"sheet3_endpoint": "SHEET3_ENDPOINT","sheet_endpoint": "SHEET_ENDPOINT","sheet_endpoint2": "SHEET_ENDPOINT2","channelM1":"CHANNEL1","channelM2":"CHANNEL2"}
+
+mainLinks = []
+
 # init web client
-client = slack_sdk.WebClient(token='YOUR TOKEN')
+client = slack_sdk.WebClient(token='TOKEN')
 
 ReminderMessage = "Hi team,\nThe bot will collect all the  client statuses tomorrow.\nMake sure every channel is updated with the client status by then.\n\nGuidelines:\n1. Should be in the main channel, NOT a thread.\n2. Should be in this format: 'Client Status: 🟢'"
 
@@ -63,10 +68,10 @@ def getTimeDifference(message_response, ts):
         temp = int(float(ts))
 
 if DayofWeek==4:
-    client.chat_postMessage(channel="C01RXTCMKLN", text=ReminderMessage)
+    # client.chat_postMessage(channel="C01RXTCMKLN", text=ReminderMessage)
     pass
 
-if DayofWeek==3:
+if DayofWeek==6:
     # gets channels that we need
     channel_ID_list = []
     channel_NAME_list = []
@@ -88,15 +93,18 @@ if DayofWeek==3:
     message_list = []
     print(channel_NAME_list)
     MessageToBeSent = ""
+    MessageToBeSent2 = ""
     for channel in channel_NAME_list:
         flag = 0
         flag2 = 0
+        flag3=0
         message_list=[]
+        message_list2=[]
         print(channel_ID_list[i])
         response = client.conversations_history(
             channel=channel_ID_list[i],
             limit=MESSAGES_PER_PAGE,
-            latest=UNIXTime,
+            oldest=UNIXTime,
         )
         assert response["ok"]
         messages_all = response['messages']
@@ -109,7 +117,8 @@ if DayofWeek==3:
             response = client.conversations_history(
                 channel=channel_ID_list[i],
                 limit=MESSAGES_PER_PAGE,
-                cursor=response['response_metadata']['next_cursor']
+                cursor=response['response_metadata']['next_cursor'],
+                oldest=UNIXTime,
             )
             assert response["ok"]
             messages = response['messages']
@@ -119,6 +128,7 @@ if DayofWeek==3:
         response = client.conversations_history(
             channel=channel_ID_list[i],
             limit=MESSAGES_PER_PAGE,
+            oldest=UNIXTime,
         )
         assert response["ok"]
         messages_all = response['messages']
@@ -131,65 +141,147 @@ if DayofWeek==3:
             response = client.conversations_history(
                 channel=channel_ID_list[i],
                 limit=MESSAGES_PER_PAGE,
-                cursor=response['response_metadata']['next_cursor']
+                cursor=response['response_metadata']['next_cursor'],
+                oldest=UNIXTime,
             )
             assert response["ok"]
             messages = response['messages']
-        # print(messages_all)
+        print(messages_all)
         
         message_list = message_list+messages_all
 
         messages_needed = []
         
         numArr = []
+        numArr2 = []
+        numArr3=[]
         get_replies(messages_all,i)
         for message_dumpp in messages_all:
-            # print(message_dumpp["text"])
-            one_message = message_dumpp["text"].lower()
-            # if (re.search(r"traffic.light:",one_message)):
-            #     print("success")
-            #     messages_needed.append("success")
-                
-            
-            
-            if (re.search(r"traffic.light:.?(:large_green_circle:|:large_yellow_circle:|:red_circle:|🔴|🟢|🟡)\ntarget.spend.\(budget\):.?([+-]?([0-9]*[.])?[0-9]+)\nactual.spend:.?([+-]?([0-9]*[.])?[0-9]+)\ntarget.cpa:.?([+-]?([0-9]*[.])?[0-9]+)\nactual.cpa:.?([+-]?([0-9]*[.])?[0-9]+)\n",one_message)):
-                flag2  = 1
-                # print(one_message)
-                # print("success")
-                messages_needed.append("success")
-                p = '[\d]+[.,\d]+|[\d]*[.][\d]+|[\d]+'
-                if (":red_circle:" in one_message):
-                    numArr.append(":red_circle:")
-                elif (":large_yellow_circle:" in one_message):
-                    numArr.append(":large_yellow_circle:")
-                elif (":red_circle:" in one_message):
-                    numArr.append(":red_circle:")
-                else:
-                    numArr.append("No status found")
-                if re.search(p, one_message) is not None:
-                    for catch in re.finditer(p, one_message):
-                        print(catch[0])
-                        numArr.append(catch[0])
-                
-                print(numArr)
-                
-            if (re.search(r"client.status.?.?.?:large_green_circle:",one_message)):
-                # print(re.search(r"Hasura",message_dumpp["text"]))
-                messages_needed.append(message_dumpp["text"])
-                flag=1
+            if float(message_dumpp["ts"])<UNIXTime:
                 break
-            elif (re.search(r"client.status.?.?.?:large_yellow_circle:",one_message)):
-                # print(re.search(r"Hasura",message_dumpp["text"]))
-                messages_needed.append(message_dumpp["text"])
-                flag=2
+            else:
+                # print("message:\n")
+                # print(message_dumpp["ts"])
+                # print("\n:message\n")
+                numArr3.clear()
+                # print(message_dumpp["text"])
+                one_message = message_dumpp["text"].lower()
+                
+                # if (re.search(r"traffic.light:",one_message)):
+                #     print("success")
+                #     messages_needed.append("success")
+                
+                
+                
+                if (re.search(r"traffic.light:.?(:large_green_circle:|:large_yellow_circle:|:red_circle:|🔴|🟢|🟡)\ntarget.spend.\(budget\):.?([+-]?([0-9]*[.])?[0-9]+)\nactual.spend:.?([+-]?([0-9]*[.])?[0-9]+)\ntarget.cpa:.?([+-]?([0-9]*[.])?[0-9]+)\nactual.cpa:.?([+-]?([0-9]*[.])?[0-9]+)",one_message)):
+                    flag2  = 1
+                    
+                    # print(one_message)
+                    # print("success")
+                    messages_needed.append("success")
+                    regexTrafficLight = '[\d]+[.,\d]+|[\d]*[.][\d]+|[\d]+'
+                    if (":red_circle:" in one_message):
+                        numArr.append(":red_circle:")
+                    elif (":large_yellow_circle:" in one_message):
+                        numArr.append(":large_yellow_circle:")
+                    elif (":red_circle:" in one_message):
+                        numArr.append(":red_circle:")
+                    else:
+                        numArr.append("No status found")
+                    if re.search(regexTrafficLight, one_message) is not None:
+                        for catch in re.finditer(regexTrafficLight, one_message):
+                            print(catch[0])
+                            numArr.append(catch[0])
+                    numArr.append(channel)
+                    print("Hello\n")
+                    print(numArr)
+                    print("Hello\n")
+                    
+                if (re.search(r"traffic.light:.?:large_green_circle:",one_message)):
+                    # print(re.search(r"Hasura",message_dumpp["text"]))
+                    messages_needed.append(message_dumpp["text"])
+                    flag=1
+                    break
+                elif (re.search(r"traffic.light:.?:large_yellow_circle:",one_message)):
+                    # print(re.search(r"Hasura",message_dumpp["text"]))
+                    messages_needed.append(message_dumpp["text"])
+                    flag=2
+                    break
+                elif (re.search(r"traffic.light:.?:red_circle:",one_message  )):
+                    # print(re.search(r"Hasura",message_dumpp["text"]))
+                    messages_needed.append(message_dumpp["text"])
+                    flag=3
+                    break
+        
+        #For second message:
+        for message_dumpp in messages_all:
+            if float(message_dumpp["ts"])<UNIXTime:
                 break
-            elif (re.search(r"client.status.?.?.?:red_circle:",one_message  )):
-                # print(re.search(r"Hasura",message_dumpp["text"]))
-                messages_needed.append(message_dumpp["text"])
-                flag=3
-                break
+            else:
+                # print(message_dumpp["text"])
+                one_message = message_dumpp["text"].lower()
+                
+                # if (re.search(r"traffic.light:",one_message)):
+                #     print("success")
+                #     messages_needed.append("success")
+                
+#                 if (re.search(r"hi1", one_message)):
+#                     print("HI HI HI HI ELSGBSOIDGBSDGPI\n\n\nGSPIAGNDSGINGNSDGINGDOH NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+                
+                if (re.search(r"sprint.status:.?(:large_green_circle:|:large_yellow_circle:|:red_circle:|🔴|🟢|🟡)\nsprint:",one_message)):
+                    flag3  = 1
+                    # numArr3=[]
+                    # print(one_message)
+                    # print("success")
+                    messages_needed.append("success")
+                    stringSprintStatus = ['sprint:(.*)week#']
+                    if (":large_green_circle:" in one_message):
+                        numArr3.append(":large_green_circle:")
+                    elif (":large_yellow_circle:" in one_message):
+                        numArr3.append(":large_yellow_circle:")
+                    elif (":red_circle:" in one_message):
+                        numArr3.append(":red_circle:")
+                    else:
+                        numArr3.append("No status found")
+                    s = one_message
+                    numArr3.append(s[s.find("sprint:")+len("sprint:"):s.rfind("week#:")])
+                    numArr3.append(s[s.find("week#:")+len("week#:"):s.rfind("leading results:")])
+                    numArr3.append(s[s.find("leading results:")+len("leading results:"):s.rfind("lagging results:")])
+                    numArr3.append(s[s.find("lagging results:")+len("lagging results:"):s.rfind("notes:")])
+                    numArr3.append(s[s.find("notes:")+len("notes:"):])
+                    
+                    # for reg in regexSprintStatus:
+                    #     if re.search(reg, one_message) is not None:
+                    #         result = re.search(reg, one_message)
+                    #         numArr2.append(result)
+                    #     else:
+                    #         numArr2.append("Not found\n")
+                    numArr3.append(channel)
 
-            
+                    numArr2.append(numArr3)
+                    
+                    if (re.search(r"sprint.status:.?:large_green_circle:",one_message)):
+                        # print(re.search(r"Hasura",message_dumpp["text"]))
+                        # messages_needed.append(message_dumpp["text"])
+                        MessageToBeSent2 = MessageToBeSent2+(f"{channel} - {numArr3[1]} :large_green_circle:\n")
+                        
+                        # flag=1
+                        # break
+                    elif (re.search(r"sprint.status:.?:large_yellow_circle:",one_message)):
+                        # print(re.search(r"Hasura",message_dumpp["text"]))
+                        # messages_needed.append(message_dumpp["text"])
+                        MessageToBeSent2 = MessageToBeSent2+(f"{channel} - {numArr3[1]} :large_yellow_circle:\n")
+                        # flag=2
+                        # break
+                    elif (re.search(r"sprint.status:.?:red_circle:",one_message)):
+                        # print(re.search(r"Hasura",message_dumpp["text"]))
+                        # messages_needed.append(message_dumpp["text"])
+                        # flag=3
+                        # break
+                        MessageToBeSent2 = MessageToBeSent2+(f"{channel} - {numArr3[1]} :red_circle:\n")
+        print("THIS IS SECOND MESSAGE: ")
+        print(numArr2)
+        print(": THIS IS SECOND MESSAGE")
         # print(flag)
         # print(messages_needed)
         
@@ -201,10 +293,22 @@ if DayofWeek==3:
             MessageToBeSent = MessageToBeSent+(f"{channel}: :red_circle:\n")
         
         #updating to google sheet
-        
-        #updating to google sheet
+        for z in numArr2:
+            
+            sheet3_inputs = {
+                "sheet3": {
+                    "companyName": f"{z[6]}",
+                    "status":f"{z[0]}",
+                    "sprint" : f"{z[1]}",
+                    "week#" : f"{z[2]}",
+                    "leadingResults" : f"{z[3]}",
+                    "laggingResults":f"{z[4]}",
+                    "notes" : f"{z[5]}",
+                }
+            }
+            sheet3_response = requests.post(testerLinks["sheet3_endpoint"], json=sheet3_inputs)
         try:
-            sheet_endpoint = "ENDPOINT 1"
+            
             sheet_inputs = {
                 "sheet1": {
                     "company": f"{channel}",
@@ -212,20 +316,21 @@ if DayofWeek==3:
                     "date" : f"{current_date}",
                 }
             }
-            sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
+            # sheet_response = requests.post(testerLinks["sheet_endpoint"], json=sheet_inputs)
         
-            print(sheet_response.text)
+            # print(sheet_response.text)
         except:
             print("error in sheet 1")
         
         
         if flag2==1:
             try:
-                sheet_endpoint2 = "ENDPOINT 2"
+                
                 
                 # try:  
                 sheet_inputs2 = {
                     "sheet2": {
+                        "channel": f"{numArr[6]}",
                         "tl": f"{numArr[0]}",
                         "ts" : f"{numArr[1]}",
                         "date" : f"{current_date}",
@@ -235,7 +340,7 @@ if DayofWeek==3:
                         "acc" : f"{numArr[5]}",
                     }
                 }
-                sheet_response2 = requests.post(sheet_endpoint2, json=sheet_inputs2)
+                # sheet_response2 = requests.post(testerLinks["sheet_endpoint2"], json=sheet_inputs2)
             except:
                 print("error in sheet 2")
             
@@ -244,13 +349,25 @@ if DayofWeek==3:
 
 
     print(MessageToBeSent)
+    print(MessageToBeSent2)
 
     maxretries = 5
     attempt = 0
 
     while attempt < maxretries:
         try:
-            client.chat_postMessage(channel="C03T226TGBG", text=MessageToBeSent)
+            # client.chat_postMessage(channel=testerLinks["channelM1"], text=MessageToBeSent)
+            pass
+        except http.client.IncompleteRead:
+            attempt += 1
+        else:
+            break
+    maxretries = 5
+    attempt = 0
+
+    while attempt < maxretries:
+        try:
+            # client.chat_postMessage(channel=testerLinks["channelM2"], text=MessageToBeSent2)
             pass
         except http.client.IncompleteRead:
             attempt += 1
